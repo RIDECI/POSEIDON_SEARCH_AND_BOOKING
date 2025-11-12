2889e1db8bb239960a08a0ecbb84a49071aced85
oscar sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-11-11 20:06:19 -0500

    Estructurado inicial

---
+++ src/main/java/edu/dosw/rideci/POSEIDON_SEARCH_AND_BOOKING/PoseidonSearchAndBookingApplication.java
@@ -0,0 +1,13 @@
+package edu.dosw.rideci.POSEIDON_SEARCH_AND_BOOKING;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoseidonSearchAndBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoseidonSearchAndBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/dosw/rideci/POSEIDON_SEARCH_AND_BOOKING/PoseidonSearchAndBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.dosw.rideci.POSEIDON_SEARCH_AND_BOOKING;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoseidonSearchAndBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
