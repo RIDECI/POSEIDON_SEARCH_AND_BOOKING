f73e3382af053063e3e80cb3d722427b60696583
Oscar Sanchez <oscar.sporras@mail.escuelaing.edu.co>
2025-11-12 14:35:36 -0500

    Implementacion pipeline CI/CD para el backend

--- src/test/java/edu/dosw/rideci/POSEIDON_SEARCH_AND_BOOKING/PoseidonSearchAndBookingApplicationTests.java
+++ src/test/java/edu/dosw/rideci/POSEIDON_SEARCH_AND_BOOKING/PoseidonSearchAndBookingApplicationTests.java
@@ -3,7 +3,9 @@ package edu.dosw.rideci.POSEIDON_SEARCH_AND_BOOKING;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+    "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class PoseidonSearchAndBookingApplicationTests {
 
 	@Test
