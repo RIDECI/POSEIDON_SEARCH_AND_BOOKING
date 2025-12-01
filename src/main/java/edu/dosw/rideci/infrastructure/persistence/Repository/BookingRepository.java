73f49342781ec83ce1bf478fcbe0481cef39f4a2
Sergio Alejandro Idarraga Torres <chechotorres498@gmail.com>
2025-12-01 18:02:44 -0500

    prueba actions

--- src/main/java/edu/dosw/rideci/infrastructure/persistence/Repository/BookingRepository.java
+++
@@ -1,8 +0,0 @@
-package edu.dosw.rideci.infrastructure.persistence.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import edu.dosw.rideci.infrastructure.persistence.entity.BookingDocument;
-
-public interface BookingRepository extends MongoRepository<BookingDocument, String> {
-}
